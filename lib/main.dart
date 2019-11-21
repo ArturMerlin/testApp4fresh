4d00d61bbcf188ee955c2da46db1385de1ebe1f8
Nikita <nikitakasturya@gmail.com>
2019-11-21 21:07:42 +0300

    Добавил шрифт SFProDisplay Добавил класс для хранения определённых TextStyle Сверстал текст "Подарки по акциям"

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:test_app_4fresh/my_app_bar.dart';
+import 'package:test_app_4fresh/my_text_style.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,14 +9,22 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: MyAppBar(height: 50),
-        backgroundColor: Colors.white,
+          appBar: MyAppBar(height: 50),
+          backgroundColor: Colors.white,
+          body: ListView(
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.only(top: 5.0, left: 16.0),
+                child: Text(
+                  'Подарки по акциям',
+                  style: MyTextStyle.headline, //  Содержит TextStyle с заданными параметрами
+                ),
+              ),
+              SizedBox(height: 10.0),
+            ],
+          )
       ),
       debugShowCheckedModeBanner: false,
     );
-
   }
 }
-
-
-
---
+++ lib/my_text_style.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class MyTextStyle {
+  static const headline = TextStyle(
+      fontFamily: 'SFProDisplay',
+      color: Colors.black,
+      fontWeight: FontWeight.bold,
+      fontSize: 21.0);
+}
